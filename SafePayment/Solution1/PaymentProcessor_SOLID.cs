37c353dc1000653171285adc3d2046706acfa3d2
Rafael Luiz <100326663+Raphox22@users.noreply.github.com>
2024-10-25 15:17:46 -0300

    Sistema Simples

---
+++ SafePayment/Solution1/PaymentProcessor_SOLID.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+/*
+ * Payment processing and validation experiment using S.O.L.I.D principles
+ * Author: Rafael Luiz Pires Lima
+*/
+
+public interface IPaymentMethod
+{
+    void ProcessPayment(decimal amount);
+}
+
+public class CreditCardPayment : IPaymentMethod
+{
+    public void ProcessPayment(decimal amount)
+    {
+        // Process credit card payment
+        Console.WriteLine($"Processing credit card payment of {amount}");
+    }
+}
+
+public class PixPayment : IPaymentMethod
+{
+    public void ProcessPayment(decimal amount)
+    {
+        // Process pix payment
+        Console.WriteLine($"Processing pix payment of {amount}");
+    }
+}
+
+public class BankTransferPayment : IPaymentMethod
+{
+    public void ProcessPayment(decimal amount)
+    {
+        // Process boleto payment
+        Console.WriteLine($"Processing boleto payment of {amount}");
+    }
+}
+
+public class PaymentProcessor
+{
+    private readonly IPaymentMethod _paymentMethod;
+
+    public PaymentProcessor(IPaymentMethod paymentMethod)
+    {
+        _paymentMethod = paymentMethod;
+    }
+
+    public void ProcessPayment(decimal amount)
+    {
+        _paymentMethod.ProcessPayment(amount);
+    }
+}
+
